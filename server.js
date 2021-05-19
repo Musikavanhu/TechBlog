3bc00d6b1b4c0f40b312916aee04550ff12f2379
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-05-18 23:31:59 -0500

    kewl

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-//setup session
 const sess = {
   secret: 'supersecretsessionsecrettext',
   cookie: { maxAge: 180000},
@@ -22,21 +20,14 @@ const sess = {
     db: sequelize
   })
 };
-
-//handlebars initialization
 const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(session(sess));
-
-//use routes
 app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
