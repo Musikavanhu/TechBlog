a266b1c446830710b14af8230c3f87ac4056ca69
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-05-18 20:10:46 -0500

    cool

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,107 @@
 const router = require('express').router();
 const sequelize = require('../config/connection');
 
-const {Post , User, Comment } = require('../models');
+const { Post, User, Comment } = require('../models');
 
-const withAuth = require()
+const withAuth = require('../utils/auth');
+
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}));
+        res.render('dashboard', {posts, loggedIn: true});
+    })
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',   
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+        }
+            },
+            {
+                model: User,
+                attributes: ['username']
+              }
+    
+    ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData){
+            res.status(400).json({message: "No posts Here"})
+        return;
+        }
+        const post = dbPostData.get({ plain : true});
+  res.render('edit-post', {post, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edituser', withAuth, (req,res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.session.user_id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({message: " no id found"});
+            return;
+        }
+        const user = dbUserData.get({ plain: true});
+    res.render('edit-user', {user, loggedIn: true});
+    })
+    .catch((err => {
+        console.log(err);
+        res.status(500).json(err);
+    }))
+});
+
+module.exports = router;
